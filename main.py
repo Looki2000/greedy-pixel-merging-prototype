4fe457c3ff2c99643f43363554a0e227b2204d03
Looki2000 <electro.brzostek@gmail.com>
2023-04-07 22:37:14 +0200

    multiple materials are now working

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pygame
 import numpy as np
 import colorsys
+import os
 
 ##### CONFIG #####
 window_size = 720
 
 pixel_count = 20 # must be a factor of window_size
 
-pixel_type = 1
+pixel_material = 1
 
 
 grid_col = (200,) * 3
-rect_col = (100, 100, 255)
 ##################
 
 if window_size % pixel_count != 0:
-    raise Exception("pixel_count must be a factor of window_size")
+    raise Exception("window_size must be divisible by pixel_count without remainder")
 
 pixel_size = window_size // pixel_count
+half_pixel_size = pixel_size / 2
 
 pixel_max_idx = pixel_count - 1
 
@@ -29,14 +30,22 @@ font = pygame.font.SysFont("Arial", 25)
 
 
 
-# pixel type colors
-pixel_type_cols = tuple(tuple(int(val * 255) for val in colorsys.hsv_to_rgb(h/360, 0.5, 1)) for h in range(0, 360, 36))
-#print(pixel_type_cols)
+# pixel material colors
+pixel_material_cols = tuple(tuple(int(val * 255) for val in colorsys.hsv_to_rgb(h/360, 0.5, 1)) for h in range(0, 360, 36))
+
+# rect material colors
+rect_cols = tuple(tuple(int(val * 255) for val in colorsys.hsv_to_rgb(h/360, 1, 0.75)) for h in range(0, 360, 36))
 
 
 # array of pixels
-map = np.zeros((pixel_count, pixel_count), dtype=np.int8)
-# rectangles generated from map using greedy voxel merging
+if "map.npy" in os.listdir():
+    map = np.load("map.npy")
+else:
+    map = np.zeros((pixel_count, pixel_count), dtype=np.int8)
+
+
+# rectangles generated from map using greedy pixel merging
+# format: [x1, y1, x2, y2, material]
 rectangles = []
 
 
@@ -44,56 +53,57 @@ rectangles = []
 def greedy_voxel_merging():
     map_copy = map.copy()
 
+    for y in range(pixel_count):        
+        last_material = 0
 
-    for y in range(pixel_count):
-        block_size_x = 0
-        block_size_y = 1
-
-        last_val = 0
-
-        block_scan_start = None
         for x in range(pixel_count):
 
-            if map_copy[y][x] == 1:
-                block_size_x += 1
+            block_ended = False
+            
+            ## end of block
+            # material switch including to air, NOT including from air
+            if last_material != 0 and map_copy[y][x] != last_material:
+                block_end_x = x
+                block_ended = True
+            # OR end of map row on some material
+            elif x == pixel_max_idx and map_copy[y][x] != 0:
+                block_end_x = x + 1
+                block_ended = True
 
-                if last_val == 0:
-                    block_scan_start = x
-                    block_scan_y = y
-                    
-                    last_val = 1
+            if block_ended:
+                block_end_y = y + 1
 
-            if (map_copy[y][x] == 0 or x == pixel_max_idx) and block_scan_start != None:
-                last_val = 0
-
-                block_scan_end = block_scan_start + block_size_x
-
-                fit = True
-                while block_scan_y <= pixel_max_idx and fit:
-                    block_scan_y += 1
-                    for val in map_copy[block_scan_y][block_scan_start:block_scan_end]:
-                        if val == 0:
-                            fit = False
+                # scanning for other rows under existing row that can be included in the block
+                scan_loop = True
+                while scan_loop:
+                    # check if all materials in row are the same as the block's material
+                    for x2 in range(block_start_x, block_end_x):
+                        if map_copy[block_end_y][x2] != last_material:
+                            scan_loop = False
                             break
                     
-                    if fit:
-                        # set the values to 0 so they are not used again
-                        map_copy[block_scan_y][block_scan_start:block_scan_end] = 0
-
-                        block_size_y += 1
-                    else:
+                    if not scan_loop:
                         break
 
-                rectangles.append((block_scan_start, y, block_size_x, block_size_y))
-                
-                block_size_x = 0
-                block_size_y = 1
-                block_scan_start = None
+                    # if the loop ended without breaking, then all materials in the row are the same as the block's material
 
-    print("====================")
-    print(rectangles)
-    print(len(rectangles))
-    print("====================")
+                    map_copy[block_end_y][block_start_x:block_end_x] = 0
+
+                    block_end_y += 1
+
+                    # if the end of the map is reached, break the loop
+                    if block_end_y == pixel_count:
+                        scan_loop = False
+
+                ## add rectangle to rectangles list
+                rectangles.append([block_start_x, y, block_end_x, block_end_y, last_material - 1])
+
+
+            # if new block just starts
+            if map_copy[y][x] != 0 and map_copy[y][x] != last_material:
+                block_start_x = x
+
+            last_material = map_copy[y][x]
 
 
 
@@ -114,7 +124,7 @@ while True:
     keys = pygame.key.get_pressed()
     for i in range(10):
         if keys[pygame.K_0 + i]:
-            pixel_type = 10 if i == 0 else i
+            pixel_material = 10 if i == 0 else i
 
 
     # pixel placement
@@ -124,12 +134,12 @@ while True:
         mouse_tile_pos = (mouse_pos[0] // pixel_size, mouse_pos[1] // pixel_size)
 
         if not mouse_pressed:
-            mouse_place_val = pixel_type if map[mouse_tile_pos[1]][mouse_tile_pos[0]] == 0 else 0
+            mouse_place_material = pixel_material if map[mouse_tile_pos[1]][mouse_tile_pos[0]] == 0 else 0
             mouse_pressed = True
         
 
         if last_mouse_tile_pos != mouse_tile_pos:
-            map[mouse_tile_pos[1]][mouse_tile_pos[0]] = mouse_place_val
+            map[mouse_tile_pos[1]][mouse_tile_pos[0]] = mouse_place_material
 
             # greedy merging in a loop for now
             rectangles = []
@@ -137,9 +147,13 @@ while True:
 
 
         last_mouse_tile_pos = mouse_tile_pos
-    else:
+    elif mouse_pressed:
         mouse_pressed = False
         last_mouse_tile_pos = None
+
+        # save map array to file
+        np.save("map.npy", map)
+        print("map saved")
 
 
     window.fill((0, 0, 0))
@@ -153,15 +167,19 @@ while True:
     for y in range(pixel_count):
         for x in range(pixel_count):
             if map[y][x] != 0:
-                pygame.draw.rect(window, pixel_type_cols[map[y][x] - 1], (x * pixel_size, y * pixel_size, pixel_size, pixel_size))
+                pygame.draw.rect(window, pixel_material_cols[map[y][x] - 1], (x * pixel_size, y * pixel_size, pixel_size, pixel_size))
 
     # draw rectangles with thickness
     for rect in rectangles:
-        pygame.draw.rect(window, rect_col, (rect[0] * pixel_size, rect[1] * pixel_size, rect[2] * pixel_size, rect[3] * pixel_size), 5)
+        #pygame.draw.rect(window, rect_col, (rect[0] * pixel_size, rect[1] * pixel_size, rect[2] * pixel_size, rect[3] * pixel_size), 5)
+        pygame.draw.rect(window, rect_cols[rect[4]], (rect[0] * pixel_size, rect[1] * pixel_size, (rect[2] - rect[0]) * pixel_size, (rect[3] - rect[1]) * pixel_size), 5)
     
     # draw pixel type text
-    text = font.render(f"Pixel type: {pixel_type}", True, pixel_type_cols[pixel_type - 1])
+    text = font.render(f"Pixel type: {pixel_material}", True, pixel_material_cols[pixel_material - 1])
     window.blit(text, (10, 5))
+
+    # ONLY FOR INSIDE FUNCTION DRAW DEBUG
+    greedy_voxel_merging()
 
     # update
     pygame.display.update()
